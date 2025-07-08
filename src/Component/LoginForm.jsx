66c8a12e96400de80cf3a6379945d1b2b03331ed
arushikakkar <aarushikakkarr31@gmail.com>
2025-07-08 18:06:03 +0530

    Formik added

--- src/Component/LoginForm.jsx
+++ src/Component/LoginForm.jsx
@@ -1,25 +1,53 @@
-import React from 'react';
-import './LoginForm.css';
-import { useFormik } from 'formik';
-import {z} from 'zod';
+import React from "react";
+import "./LoginForm.css";
 import { FaUser, FaLock } from "react-icons/fa";
+import { useFormik } from "formik";
+import { z } from "zod";
 
-const LoginForm =() => {
+const initialValues = {
+  email: "",
+  password: "",
+};
+
+const LoginForm = () => {
+  const { values, handleBlur, handleChange, handleSubmit } = useFormik({
+    initialValues,
+    onSubmit: (values) => {
+      console.log("Form submitted with values:", values);
+    },
+  });
+
   return (
-    <div className='wrapper'>
+    <div className="wrapper">
       <h1>LoginForm</h1>
-      <form className='login-form'>
-        <div className='form-group'>
-        <input type='email' id='email' name='email' placeholder='Enter your email' />
-        <FaUser className='icon' />
+      <form className="login-form" onSubmit={handleSubmit}>
+        <div className="form-group">
+          <input
+            type="email"
+            id="email"
+            name="email"
+            placeholder="Enter your email"
+            value={values.email}
+            onBlur={handleBlur}
+            onChange={handleChange}
+          />
+          <FaUser className="icon" />
         </div>
-        <div className='form-group'>
-        <input type='password' id='password' name='password' placeholder='Enter your password' />
-        <FaLock className='icon' />
+        <div className="form-group">
+          <input
+            type="password"
+            id="password"
+            name="password"
+            placeholder="Enter your password"
+            value={values.password}
+            onBlur={handleBlur}
+            onChange={handleChange}
+          />
+          <FaLock className="icon" />
         </div>
-        <button type='submit'>Login</button>
+        <button type="submit">Login</button>
       </form>
     </div>
-  )
-}
+  );
+};
 export default LoginForm;
